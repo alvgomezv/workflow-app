7991982e6044dc7c3fd45be0fd7b6e74d0fa9c87
Alvaro Gomez <alkitegv@gmail.com>
2024-09-06 11:26:23 +0200

    Installed prettier for formatting

--- src/components/CircleComponent.jsx
+++ src/components/CircleComponent.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import Svg, { Circle } from "react-native-svg";
 
-export default function CircleComponent() {
-    return (
-      <View style={styles.container}>
-        <Svg height="200" width="200" viewBox="0 0 100 100">
-          <Circle
-            cx="50"
-            cy="50"
-            r="45"
-            stroke="black"
-            strokeWidth="1"
-            fill="green"
-          />
-        </Svg>
-      </View>
-    );
-  }
+export default function CircleComponent({ fillColor }) {
+  return (
+    <View style={styles.container}>
+      <Svg height="200" width="200" viewBox="0 0 100 100">
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="black"
+          strokeWidth="1"
+          fill={fillColor}
+        />
+      </Svg>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
